70d5428c389a30436ed8f790b916cab8c76fe0be
ali <xuyafee@gmail.com>
2014-03-18 11:59:30 +0100

    update2014-03-18-1159

--- quarterlyIssuance.R
+++ quarterlyIssuance.R
@@ -1,16 +1,17 @@
-d2008=read.csv("C:/+++++CDODATA/usingDataSet/2008quarter.csv") # read data
+# read data 2008quarter.csv
+d2008=read.csv("C:/+++++CDODATA/usingDataSet/2008quarter.csv")
 
-plot(as.Date(d2008[,2]),as.numeric(d2008[,6]), # do plot
-xlab="Quarterly Date", ylab="USD Millions",
+# do plot
+plot(as.Date(d2008[,2]),as.numeric(d2008[,6]), xlab="Quarterly Date", ylab="USD Millions",
 type="b",col="blue",pch=17)
 abline(v=as.Date(d2008[,2])[7],col="red")
 abline(v=as.Date(d2008[,2])[21],col="green")
 abline(v=as.Date(d2008[,2])[24],col="black")
 text(as.Date(d2008[,2])[7], as.numeric(d2008[,6])[7]+3000,
- as.numeric(d2008[,6])[7])
+as.numeric(d2008[,6])[7])
 text(as.Date(d2008[,2])[21], as.numeric(d2008[,6])[21]-1000,
- as.numeric(d2008[,6])[21])
+as.numeric(d2008[,6])[21])
 text(as.Date(d2008[,2])[24]-100, as.numeric(d2008[,6])[24]-1000,
- as.numeric(d2008[,6])[24])
+as.numeric(d2008[,6])[24])
 
 
--- yearlyIssuance.R
+++ yearlyIssuance.R
@@ -1,15 +1,16 @@
-d2000=read.csv("C:/+++++CDODATA/usingDataSet/2000year.csv") # read data
+# read data 2000year.csv
+d2000=read.csv("C:/+++++CDODATA/usingDataSet/2000year.csv") 
 
-plot(as.Date(d2000[,1]),as.numeric(d2000[,6]), # do plot
-xlab="Yearly Date", ylab="USD Millions",
+# do plot
+plot(as.Date(d2000[,1]),as.numeric(d2000[,6]), xlab="Yearly Date", ylab="USD Millions",
 type="b",col="blue",pch=17)
 abline(v=as.Date(d2000[,1])[10],col="red")
 abline(v=as.Date(d2000[,1])[7],col="green")
 abline(v=as.Date(d2000[,1])[14],col="black")
 text(as.Date(d2000[,1])[10], as.numeric(d2000[,6])[10]+20000,
- as.numeric(d2000[,6])[10])
+as.numeric(d2000[,6])[10])
 text(as.Date(d2000[,1])[7], as.numeric(d2000[,6])[7]-15000,
- as.numeric(d2000[,6])[7])
+as.numeric(d2000[,6])[7])
 text(as.Date(d2000[,1])[14], as.numeric(d2000[,6])[14]+25000,
- as.numeric(d2000[,6])[14])
+as.numeric(d2000[,6])[14])
 
