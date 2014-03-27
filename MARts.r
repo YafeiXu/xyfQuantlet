3c2fae7e7d59456ef417280c080581b0c4e06d63
YafeiXu <xuyafee@gmail.com>
2014-03-27 14:34:32 +0100

    327

--- MARts.r
+++ MARts.r
@@ -1,14 +1,20 @@
-# MARgss
-# 10000 Monte Carlo simulations for 3-dimensional Gaussian copula with exchangeable correlation matrix
+# MARts
 library(copula)
 library(rgl)
+
+# t copula
 M=10000 # number of Monte Carlo simulation
 rho=.9 # single parameter in correlation matrix of the 3-dimensional Gaussian copula
 d=3 # dimension of the Gaussian copula
-norm.cop <- normalCopula(rho, dim = d, dispstr = "ex") 
-U <- rCopula(M, norm.cop) 
+dOfF=3 # degree of freedom
+theta1Input=0.7 # parameter in t-copula
+
+t.cop1 <- tCopula(theta1Input, dim = d, dispstr = "ex",df = 3, df.fixed = TRUE)
+U <- rCopula(M, t.cop1 ) 
 u1=U[,1]
 u2=U[,2]
 u3=U[,3]
 
 plot3d(u1, u2, u3,col=4, size=.2, type='s') # draw plot
+
+
